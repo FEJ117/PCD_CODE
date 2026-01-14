e22e91fc10b50a15f7878aec9f204e4cdf626100
FEJ117 <FEJ117@users.noreply.github.com>
2026-01-14 14:55:50 +0000

    deploy: f9d58a68dedbdee9a9845ac21922d145de6867c3

--- search/all_10.js
+++ search/all_10.js
@@ -4,10 +4,9 @@ var searchData=
   ['pendsv_5fhandler_1',['PendSV_Handler',['../stm32f0xx__it_8c.html#a6303e1f258cbdc1f970ce579cc015623',1,'stm32f0xx_it.c']]],
   ['position_20after_20beg_2',['If the condition is true: Jump into the block (one position after BEG)',['../InstructionList_8c.html#autotoc_md3',1,'']]],
   ['position_20after_20end_3',['If the condition is false: Jump one position after END',['../InstructionList_8c.html#autotoc_md4',1,'']]],
-  ['programmelemente_4',['STM32-Spezifische Programmelemente',['../index.html#autotoc_md2',1,'']]],
-  ['programmingmode_5',['programmingmode',['../InstructionList_8c.html#ac032f6d5132ab94719acb7891e553bdf',1,'programmingMode(void):&#160;InstructionList.c'],['../InstructionList_8h.html#ac032f6d5132ab94719acb7891e553bdf',1,'programmingMode(void):&#160;InstructionList.c']]],
-  ['programmlogik_6',['Programmlogik',['../index.html#autotoc_md1',1,'']]],
-  ['ps2driver_2ec_7',['PS2Driver.c',['../PS2Driver_8c.html',1,'']]],
-  ['ps2driver_2eh_8',['PS2Driver.h',['../PS2Driver_8h.html',1,'']]],
-  ['putinstruction_9',['putinstruction',['../BA__EEPROM_8c.html#a054467629e212e68ea643f35cabf8dd6',1,'putInstruction(Instruction in, int position):&#160;BA_EEPROM.c'],['../BA__EEPROM_8h.html#a054467629e212e68ea643f35cabf8dd6',1,'putInstruction(Instruction in, int position):&#160;BA_EEPROM.c']]]
+  ['programmingmode_4',['programmingmode',['../InstructionList_8c.html#ac032f6d5132ab94719acb7891e553bdf',1,'programmingMode(void):&#160;InstructionList.c'],['../InstructionList_8h.html#ac032f6d5132ab94719acb7891e553bdf',1,'programmingMode(void):&#160;InstructionList.c']]],
+  ['programs_20main_20logic_5',['The programs main logic',['../index.html#autotoc_md1',1,'']]],
+  ['ps2driver_2ec_6',['PS2Driver.c',['../PS2Driver_8c.html',1,'']]],
+  ['ps2driver_2eh_7',['PS2Driver.h',['../PS2Driver_8h.html',1,'']]],
+  ['putinstruction_8',['putinstruction',['../BA__EEPROM_8c.html#a054467629e212e68ea643f35cabf8dd6',1,'putInstruction(Instruction in, int position):&#160;BA_EEPROM.c'],['../BA__EEPROM_8h.html#a054467629e212e68ea643f35cabf8dd6',1,'putInstruction(Instruction in, int position):&#160;BA_EEPROM.c']]]
 ];
--- search/all_d.js
+++ search/all_d.js
@@ -1,9 +1,10 @@
 var searchData=
 [
   ['main_0',['main',['../main_8c.html#a840291bc02cba5474a4cb46a9b9566fe',1,'main.c']]],
-  ['main_2ec_1',['main.c',['../main_8c.html',1,'']]],
-  ['mx_5fadc_5finit_2',['MX_ADC_Init',['../main_8c.html#a6b0e7d15a108faae0b676d92ccef6fce',1,'main.c']]],
-  ['mx_5fgpio_5finit_3',['MX_GPIO_Init',['../main_8c.html#ae89fdd15729ad41a66911190fcbab23a',1,'main.c']]],
-  ['mx_5fi2c1_5finit_4',['MX_I2C1_Init',['../main_8c.html#a4230d11f81d0b16e112f1cc3f40cb42f',1,'main.c']]],
-  ['mx_5ftim14_5finit_5',['MX_TIM14_Init',['../main_8c.html#aeedab6b0210385bf4ee7cbcf461c3395',1,'main.c']]]
+  ['main_20logic_1',['The programs main logic',['../index.html#autotoc_md1',1,'']]],
+  ['main_2ec_2',['main.c',['../main_8c.html',1,'']]],
+  ['mx_5fadc_5finit_3',['MX_ADC_Init',['../main_8c.html#a6b0e7d15a108faae0b676d92ccef6fce',1,'main.c']]],
+  ['mx_5fgpio_5finit_4',['MX_GPIO_Init',['../main_8c.html#ae89fdd15729ad41a66911190fcbab23a',1,'main.c']]],
+  ['mx_5fi2c1_5finit_5',['MX_I2C1_Init',['../main_8c.html#a4230d11f81d0b16e112f1cc3f40cb42f',1,'main.c']]],
+  ['mx_5ftim14_5finit_6',['MX_TIM14_Init',['../main_8c.html#aeedab6b0210385bf4ee7cbcf461c3395',1,'main.c']]]
 ];
