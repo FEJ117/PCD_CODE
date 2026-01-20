5e8605e15495f06849ebcd41622accb33e078255
fabio_elias.jain <fabio_elias.jain@smail.th-koeln.de>
2026-01-20 15:23:45 +0100

    Made changes to the first example

--- Core/Src/InstructionHandlers.h
+++ Core/Src/InstructionHandlers.h
@@ -28,7 +28,15 @@ void InstructionHandlers_ProcessData(Instruction *exe);
 
 
 /** @brief Handler for the instructions EMP, BEG and END. Does nothing. 
-  * @example The code:\INH 0\BEG\LD1 V\WAI 5\LD1 A\WAI 5\END\JUM 0\
+  * @example BEG/END The code: \n
+  * INH 0   \n
+  * BEG     \n
+  * LD1 V   \n
+  * WAI 5   \n
+  * LD1 A   \n
+  * WAI 5   \n
+  * END     \n
+  * JUM 0   \n
   * will let the left LED blink when the first Button is pressed. Everything between BEG and END
   * is only being executed, if the condition (INH 0) is true.
 */
